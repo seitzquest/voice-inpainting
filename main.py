5cddb3c85c37235fc2ea41ac9fb26b72623ef729
Philipp Seitz <philipp_seitz@outlook.com>
2025-03-30 22:03:59 +0200

    Small backend fixes

--- main.py
+++ main.py
@@ -465,4 +465,4 @@ async def health_check():
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
